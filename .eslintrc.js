ca5d48633901f2efb272bea29befe1a75f4270c7
yhostc <yhostc@gmail.com>
2024-03-23 18:49:48 +0800

    build: upgrade to pnpm workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,10 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  extends: ['plugin:prettier/recommended'],
+  ignorePatterns: ['.eslintrc.js', 'dist'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 120,
-      },
-    ],
+    'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
+    'max-params': ['error', 3],
+    'max-lines-per-function': ['error', 70],
   },
 };
