32f5a2abbdf691de87946235948ee8d442ac3454
yhostc <yhostc@gmail.com>
2024-04-07 08:35:36 +0800

    chore: @ApiExcludeController() for notify controller

--- packages/payment/src/subscription/subscription.controller.ts
+++ packages/payment/src/subscription/subscription.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 
 import { AppleGuard, GoogleGuard, StripeGuard } from './guards';
 import { SubscriptionService } from './subscription.service';
@@ -8,6 +9,7 @@ export interface RequestWithNotice extends Request {
 }
 
 @Controller('notify')
+@ApiExcludeController()
 export class SubscriptionController {
   constructor(private service: SubscriptionService) {}
 
